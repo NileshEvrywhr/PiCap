d4b4a3db09d721b451548ecedeb2b09a89fa8676
nileshk-g <nilesh.guptain@gmail.com>
2020-05-12 19:19:55 +0530

    setup changes;

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ sudo apt install python3-pip
 sudo pip3 install -U pip
 pip install pipenv
 
-pipenv shell && \
-pipenv run pip install --upgrade --timeout=3600 apache-beam[gcp] oauth2client==3.0.0
+pipenv shell
+pipenv install apache-beam[gcp] oauth2client==3.0.0
